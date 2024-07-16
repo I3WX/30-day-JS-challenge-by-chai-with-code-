a4476e2a15ee9c3c054273b98b1ef7963ed8db75
I3WX <utkarshs7828@gmail.com>
2024-07-16 15:54:25 +0530

    Number Printing Script

---
+++ day4/numberPrint.js
@@ -0,0 +1,16 @@
+function forLoopNum(){
+    for (let num = 1; num <= 10; num++) {
+        console.log(num)
+    }
+}
+
+function whileLoopNum(){
+    let x = 1 ;
+    while (x <= 10){
+        console.log(x)
+        x++;
+    }
+}
+
+forLoopNum()
+whileLoopNum()
