8509ec5613473c3aa8c8f99140da68e0e5278c91
I3WX <utkarshs7828@gmail.com>
2024-07-16 16:22:20 +0530

    Sum Calculation Script

---
+++ day4/PatternPrintingScrip.js
@@ -0,0 +1,9 @@
+function printPattern(){
+    for (let i = 1; i < 5; i++) {
+        for (let j = 0; j < i; j++) {
+            console.log("*")
+        }
+    }
+}
+
+printPattern()
---
+++ day4/SumCalculationScript.js
@@ -0,0 +1,12 @@
+function sumNumber(){
+    let num = 1
+    let sum = 0
+    while(num <= 10){
+        console.log(num)
+        sum += num
+        num++
+    }
+    console.log(sum)
+}
+
+sumNumber()
