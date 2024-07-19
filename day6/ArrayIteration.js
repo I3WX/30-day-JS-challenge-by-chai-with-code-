8f4fa404f9891db989a640a97da0ea8a94f55995
I3WX <utkarshs7828@gmail.com>
2024-07-19 12:34:03 +0530

    .

--- day6/ArrayIteration.js
+++ day6/ArrayIteration.js
@@ -1,10 +1,15 @@
-const Array = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
+const array = [] 
+
+for(let i = 0 ; i<=10 ; i++){
+    let num = Math.floor(Math.random() * 100)
+    array.push(num)
+}
 
 console.log(`Array Iteration using for loop`)
-for (let i = 0; i < Array.length; i++) {
+for (let i = 0; i < array.length; i++) {
     console.log(i)
 }
 console.log(`Array Iteration using foreach`)
-Array.forEach(num => {
+array.forEach(num => {
     console.log(num)
 });
