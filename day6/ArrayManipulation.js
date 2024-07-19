d73f4928f3ad965e45dc8fd9aa84e75557669452
I3WX <utkarshs7828@gmail.com>
2024-07-19 11:05:37 +0530

    Array Manipulation Script

---
+++ day6/ArrayManipulation.js
@@ -0,0 +1,11 @@
+let Arr = [0,1,2,3,4,5,6,7,8,9]
+
+console.log(` Original Array : ${Arr}`)
+Arr.push(5)
+console.log(` Push 5 in Array : ${Arr}`)
+Arr.pop(5)
+console.log(` Pop 5 in Array : ${Arr}`)
+Arr.shift(3)
+console.log(` Shift 3 in Array : ${Arr}`)
+Arr.unshift(6)
+console.log(` Unshift 6 in Array : ${Arr}`)
